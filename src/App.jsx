239a863a3d15b2bf2aae0adf27e729f127f2f398
Beatus Simon <beatussimon49@gmail.com>
2025-01-17 20:33:31 +0300

    Use the use reducer hook to impelement the count app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
+import { useReducer } from "react";
+import { useState } from "react";
+
 function App(){
+    const[count, setCount] = useState(0)
+
+    const[state, dispatch] =useReducer(reducer, 0);
+
+    function reducer(state, action){
+        if(action.type =='increment'){
+            return state+action.payload;
+        }
+        else if(action.type =='decrement'){
+            return state-action.payload;
+        }
+    }
+
     return <div className="App">
-        
+        useState hook: {count}
+        <button onClick={()=>setCount(count+1)}>Increment</button>
+        <button onClick={()=>setCount(count-1)}>Decrement</button>
+
+        useReducer hook: {state}
+        <button onClick={()=>dispatch({type: 'increment', payload:1})}>Increment</button>
+        <button onClick={()=>dispatch({type: 'decrement', payload:1})}>Decrement</button>
     </div>
 }
 
